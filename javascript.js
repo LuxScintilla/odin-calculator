40f77253dfcc9c6786e991de539b8d92fbffea26
LuxScintilla <catherina.ea@gmail.com>
2023-03-18 22:15:21 +0000

    Fully functional except Negatives

--- javascript.js
+++ javascript.js
@@ -9,13 +9,14 @@ const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator")
 const dotButton = document.querySelector(".dot");
 
-let memory = {};
+const regex = /[\*+\-\/]/g
 
 let fields;
 let firstNumber;
 let secondNumber;
 let seperator;
 let operator;
+let sum;
 
 function inputDisplay() {
     inputBox.textContent += this.dataset.value;
@@ -25,16 +26,34 @@ for(let button of numberButtons) {
     button.addEventListener("click", inputDisplay)
 }
 
+let dotClicked = 0;
+function dotFunction() {
+    inputBox.textContent += this.dataset.value;
+    dotClicked++
+    if(dotClicked == 1) {
+        dotButton.removeEventListener("click", dotFunction);
+    }
+}
+
+dotButton.addEventListener("click", dotFunction);
+
 let clicked = 0;
 for(let button of operatorButtons) {
     button.addEventListener("click", function() {
         inputBox.textContent += this.dataset.value;
         clicked++
+        dotClicked = 0;
+        dotButton.addEventListener("click", dotFunction);
 
         if(clicked >= 2) {
             checkInput()
             inputBox.textContent += this.dataset.value;
-            outputBox.textContent = operate(operator, firstNumber, secondNumber);
+            sum = operate(operator, firstNumber, secondNumber);
+            if(sum % 1 != 0) {
+                outputBox.textContent = sum.toFixed(2);
+            } else {
+                outputBox.textContent = sum.toFixed();
+            }
         }
 
     });
@@ -42,23 +61,41 @@ for(let button of operatorButtons) {
 
 function checkInput() {
 
-    fields = inputBox.textContent.split(/[.\*+-/_]/);
+    console.log(inputBox.textContent);
+
+    fields = inputBox.textContent.split(regex);
 
     console.log(fields);
 
     firstNumber = parseFloat(fields[0]);
     secondNumber = parseFloat(fields[1]);
-
-    operator = inputBox.textContent.slice((fields[0].length), (fields[0].length + 1));
+    operator = inputBox.textContent.slice((fields[0].length), (fields[0].length+1));
+    console.log(firstNumber);
+    console.log(secondNumber);
     console.log(operator);
-    inputBox.textContent = operate(operator, firstNumber, secondNumber);
+    sum = operate(operator, firstNumber, secondNumber);
+    if(sum % 1 != 0) {
+        inputBox.textContent = sum.toFixed(2);
+    } else {
+        inputBox.textContent = sum.toFixed();
+    }
+
 }
 
 equalsButton.addEventListener("click", function() {
 
+    clicked = 0;
+    dotClicked = 0;
+    dotButton.addEventListener("click", dotFunction);
+
     checkInput()
 
-    outputBox.textContent = operate(operator, firstNumber, secondNumber);
+    sum = operate(operator, firstNumber, secondNumber);
+    if(sum % 1 != 0) {
+        outputBox.textContent = sum.toFixed(2);
+    } else {
+        outputBox.textContent = sum.toFixed();
+    }
 
     inputBox.textContent = outputBox.textContent;
 });
