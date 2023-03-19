b224c70debbc5f2668ee06efe9a381fd7c2cd385
LuxScintilla <catherina.ea@gmail.com>
2023-03-19 22:19:02 +0000

    Fix input validation, negatives still broken

--- javascript.js
+++ javascript.js
@@ -18,81 +18,120 @@ let seperator;
 let operator;
 let sum;
 
-function inputDisplay() {
+function displayInput() {
     inputBox.textContent += this.dataset.value;
 }
 
+//NUMBER BUTTONS-------------------------------------
+let numberClicked = false;
+function numberFunction() {
+    inputBox.textContent += this.dataset.value;
+    numberClicked = true;
+}
 for(let button of numberButtons) {
-    button.addEventListener("click", inputDisplay)
+    button.addEventListener("click", numberFunction);
 }
 
-let dotClicked = 0;
+//DOT BUTTON-----------------------------------------
+let dotClicked = false;
 function dotFunction() {
-    inputBox.textContent += this.dataset.value;
-    dotClicked++
-    if(dotClicked == 1) {
-        dotButton.removeEventListener("click", dotFunction);
+    if(numberClicked == true) {
+        inputBox.textContent += this.dataset.value;
+        dotClicked = true;
+        if(dotClicked == true) {
+            dotButton.removeEventListener("click", dotFunction);
+        } else if(dotClicked == false) {
+            dotButton.addEventListener("click", dotFunction);
+        }
     }
 }
-
 dotButton.addEventListener("click", dotFunction);
 
-let clicked = 0;
-for(let button of operatorButtons) {
-    button.addEventListener("click", function() {
-        inputBox.textContent += this.dataset.value;
-        clicked++
-        dotClicked = 0;
-        dotButton.addEventListener("click", dotFunction);
-
-        if(clicked >= 2) {
-            checkInput()
-            inputBox.textContent += this.dataset.value;
-            sum = operate(operator, firstNumber, secondNumber);
-            if(sum % 1 != 0) {
-                outputBox.textContent = sum.toFixed(2);
-            } else {
-                outputBox.textContent = sum.toFixed();
-            }
-        }
-
-    });
-}
-
-function checkInput() {
-
-    console.log(inputBox.textContent);
-
-    fields = inputBox.textContent.split(regex);
-
-    console.log(fields);
-
-    firstNumber = parseFloat(fields[0]);
-    secondNumber = parseFloat(fields[1]);
-    operator = inputBox.textContent.slice((fields[0].length), (fields[0].length+1));
-    console.log(firstNumber);
-    console.log(secondNumber);
-    console.log(operator);
-    sum = operate(operator, firstNumber, secondNumber);
-    if(sum % 1 != 0) {
-        inputBox.textContent = sum.toFixed(2);
-    } else {
-        inputBox.textContent = sum.toFixed();
+//OPERATOR BUTTONS------------------------------------
+let operatorClicked = 0;
+function operatorFunction() {
+    if(inputBox.textContent.slice(-1) == ".") {
+        numberClicked = false;
     }
 
+    if(numberClicked == true) {
+        inputBox.textContent += this.dataset.value;
+        operatorClicked++
+        numberClicked = false;
+        dotClicked = false;
+        dotButton.addEventListener("click", dotFunction);
+    }
+    if(operatorClicked > 1) {
+        numberClicked = false;
+        dotClicked = false;
+        dotButton.addEventListener("click", dotFunction);
+        calculateInput()
+        inputBox.textContent += this.dataset.value;
+        operatorClicked = 1;
+    }
+    console.log(operatorClicked);
 }
 
-equalsButton.addEventListener("click", function() {
+for(let button of operatorButtons) {
+    button.addEventListener("click", operatorFunction);
+}
 
-    clicked = 0;
-    dotClicked = 0;
+//CHECK INPUT FOR CALCULATION---------------------------
+let sliced;
+function calculateInput() {
+    //SNARKY ERROR MESSAGE-------------------------------
+    if(inputBox.textContent == "0/0") {
+        inputBox.textContent = "To Infinity";
+        outputBox.textContent = "and Beyond";
+    }
+    if(Math.sign(outputBox.textContent) == -1) {
+
+        fields = inputBox.textContent.split(regex);
+
+        //FIND A WAY TO FIX NEGATIVES!!!
+
+        firstNumber = parseFloat(fields[0]);
+        secondNumber = parseFloat(fields[1]);
+        operator = inputBox.textContent.slice((fields[0].length), (fields[0].length+1));
+
+        sum = operate(operator, firstNumber, secondNumber);
+        if(sum % 1 != 0) {
+            outputBox.textContent = sum.toFixed(1);
+        } else {
+            outputBox.textContent = sum.toFixed();
+        }
+        inputBox.textContent = outputBox.textContent;
+
+    } else {
+        fields = inputBox.textContent.split(regex);
+
+        console.log(fields);
+
+        firstNumber = parseFloat(fields[0]);
+        secondNumber = parseFloat(fields[1]);
+        operator = inputBox.textContent.slice((fields[0].length), (fields[0].length+1));
+
+        sum = operate(operator, firstNumber, secondNumber);
+        if(sum % 1 != 0) {
+            outputBox.textContent = sum.toFixed(1);
+        } else {
+            outputBox.textContent = sum.toFixed();
+        }
+        inputBox.textContent = outputBox.textContent;
+    }
+}
+
+//EQUALS BUTTON-----------------------------------------
+equalsButton.addEventListener("click", function() {
+    operatorClicked = 0;
+    dotClicked = false;
     dotButton.addEventListener("click", dotFunction);
 
-    checkInput()
+    calculateInput()
 
     sum = operate(operator, firstNumber, secondNumber);
     if(sum % 1 != 0) {
-        outputBox.textContent = sum.toFixed(2);
+        outputBox.textContent = sum.toFixed(1);
     } else {
         outputBox.textContent = sum.toFixed();
     }
@@ -100,6 +139,7 @@ equalsButton.addEventListener("click", function() {
     inputBox.textContent = outputBox.textContent;
 });
 
+//CLEAR BUTTON--------------------------------------------
 clearButton.addEventListener("click", function() {
 
     inputBox.textContent = "";
@@ -108,15 +148,41 @@ clearButton.addEventListener("click", function() {
     firstNumber = "";
     secondNumber = "";
     operator = "";
-    clicked = 0;
+    numberClicked = false;
+    dotClicked = false;
+    dotButton.addEventListener("click", dotFunction);
+    operatorClicked = 0;
+    poppedArray = []
 });
 
+//BACKSPACE BUTTON----------------------------------------
+let pop;
+let poppedArray = [];
 backspaceButton.addEventListener("click", function() {
     let inputArray = inputBox.textContent.split("");
+    pop = inputBox.textContent.slice(-1);
+    poppedArray.push(pop);
     inputArray.pop();
     inputBox.textContent = inputArray.join("");
+
+    console.log(poppedArray);
+    for(let item of poppedArray) {
+        if(item.match(".") == ".") {
+            poppedArray = [];
+            dotClicked = false;
+            dotButton.addEventListener("click", dotFunction);
+        }
+        if(regex.test(item) == true) {
+            if(typeof parseFloat(inputBox.textContent.slice(-1)) == "number") {
+                poppedArray = [];
+                operatorClicked = 0;
+                numberClicked = true;
+            }
+        }
+    }
 });
 
+//CALCULATION FUNCTIONS-----------------------------------
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
